f10585af74bbbb3c37d7aeffc510747a46a5d88f
Constantin Wolf <Constantin Wolf>
2024-03-16 13:38:26 +0100

    added OTA possibility on every boot

--- readme.md
+++ readme.md
@@ -6,9 +6,11 @@ This project is based of [pschatzmann/arduino-snapclient](https://github.com/psc
 - [x] wifi network loudspeaker which receives data from a [Snapcast server](https://github.com/badaix/snapcast)
 - [x] ESP32 use ethernet instead of WiFi (WT32-eth01)
 - [x] interface with low power I2S amplifier (MAX98357A)
+- [x] OTA via ethernet (only possible shortly after boot)
 - [ ] interface with high power I2S amplifier (TAS5713)
 - [ ] synchronize with other speakers
 - [ ] create PCB to amp-board powering everything from one power supply 12-20V
 
 # optional
 - [ ] RP2040 use ethernet (W5500-EVB-PICO, RP2040-ETH Mini)
+- [ ] allow OTA at all times (see [RTOS example](https://github.com/SensorsIot/ESP32-OTA/issues/8#issuecomment-689524162))
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include "AudioCodecs/CodecOpus.h" // https://github.com/pschatzmann/arduino-libopus
 #ifdef CONFIG_ETHERNET
 #include <ETH.h>
+#include <SPI.h>
+#include <ESPmDNS.h>                   // Bonjour/multicast DNS, finds the device on network by name   
+#include <ArduinoOTA.h>                // OTA Upload via ArduinoIDE
 #endif
 // AudioBoardStream out(AudioKitEs8388V1);  // or replace with e.g. 
 I2SStream out;
@@ -16,9 +19,13 @@ SnapProcessorRTOS rtos(1024*8); // define queue with 8 kbytes
 SnapTimeSyncDynamic synch(0, 10); // optional configuratioin
 SnapClient client(wifi, out, opus);
 
+// allows OTA only shortly after boot
+#define MAX_INITIAL_OTA_TIME 5000
+
 #ifdef CONFIG_ETHERNET
 static bool eth_connected = false;
 static bool eth_reboot_on_reconnect = false;
+
 void EthernetEvent(WiFiEvent_t event)
 {
   switch (event) {
@@ -58,6 +65,46 @@ void EthernetEvent(WiFiEvent_t event)
       break;
   }
 }
+
+void ArduinoOTASetup() {
+  //IDE OTA
+#ifdef ARDUINO_ARCH_ESP32
+  char myhostname[16] {"esp"};         // you could use your TXT_BOARDNAME also. The sketch will add the board ID.
+  char val[8];
+  itoa(1, val, 10);            // before we can concatenate the precompiler integer, we have to convert it to char array
+  strcat(myhostname, val);
+  ArduinoOTA.setHostname(myhostname);  // give a name to your ESP for the Arduino IDE - not needed for ESP8266
+#endif
+  ArduinoOTA.begin();                  // OTA Upload via ArduinoIDE https://arduino-esp8266.readthedocs.io/en/latest/ota_updates/readme.html
+  // OTA Messages
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA Start");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    static byte previous = 0;
+    byte result = progress / (total / 100);
+    if (previous != result) {
+      if ((result % 10) == 0) {
+        Serial.print(result);
+        Serial.println("%");
+      }
+      else Serial.print(".");
+    }
+    previous = result;
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA End");
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("\nOTA Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("\nAuth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("\nBegin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("\nConnect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("\nReceive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("\nEnd Failed");
+  });
+}
+
 #endif
 
 void setup() {
@@ -82,6 +129,9 @@ void setup() {
     Serial.print('.');
     delay(1000);
   }
+
+  ETH.setHostname("esp32-ethernet");
+  ArduinoOTASetup();
 #endif
 
   auto cfg = out.defaultConfig();
@@ -110,6 +160,14 @@ void setup() {
 
   // start snap client
   client.begin(synch);
+
+  Serial.println("Allow OTA updates"); 
+  long ota_start_time = millis();
+  while(millis()-ota_start_time < MAX_INITIAL_OTA_TIME) {
+    ArduinoOTA.handle();                 // OTA Upload via ArduinoIDE
+    delay(10);
+  }
+  Serial.println("Stop allowing OTA updates");
 }
 
 void loop() {
